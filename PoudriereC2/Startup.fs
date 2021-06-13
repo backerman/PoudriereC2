a8648189fcb8538c8622148a07b95aa91dbdca1d
Brad Ackerman <brad@facefault.org>
2021-06-12 21:04:08 -0700

    Remove unused file.

--- PoudriereC2/Startup.fs
+++
@@ -1,6 +0,0 @@
-module Facefault.PoudriereC2.AssemblyMetadata
-open Microsoft.Azure.Functions.Extensions.DependencyInjection
-
-[<assembly:FunctionsStartup(typeof<Configuration.Startup>)>]
-do
-    printfn "Configuring singletons."
