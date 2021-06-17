86f33ccc6b2bac685348d5bf750aa838427e62a1
Brad Ackerman <brad@facefault.org>
2021-06-16 21:12:33 -0700

    Basic unit tests for media type parsing.

---
+++ PoudriereC2.Tests/MediaType.fs
@@ -0,0 +1,47 @@
+module PoudriereC2.Tests.MediaType
+
+open NUnit.Framework
+open FsUnit
+open AutoFixture
+open AutoFixture.AutoFoq
+open System
+open Facefault.PoudriereC2.Serialization
+open Microsoft.Azure.Functions.Worker.Http
+open System.IO
+open AutoFixture.Kernel
+
+type ConcreteRequestData(functionContext) =
+    inherit HttpRequestData(functionContext)
+    override val Body =
+        new MemoryStream() :> Stream with get
+    override val Cookies =
+        [] :> Collections.Generic.IReadOnlyCollection<IHttpCookie> with get
+    override this.CreateResponse(): HttpResponseData = 
+        failwith "Not Implemented"
+    override val Headers = new HttpHeadersCollection() with get
+    override val Identities = 
+        [] |> Seq.ofList with get
+    override val Method = "GET" with get
+    override val Url = 
+        Uri("http://localhost") with get
+
+[<TestFixture>]
+type MediaTypeHandlerTests() =
+    [<Test>]
+    member __.TestGoodValues() =
+        let fixture = Fixture()
+        new AutoFoqCustomization()
+        |> fixture.Customize
+        |> ignore
+        fixture.Customizations.Add(
+            TypeRelay(typeof<HttpRequestData>, typeof<ConcreteRequestData>))
+        [{|MediaType = "application/json"; Result = Some Json|};
+         {|MediaType = "text/plain"; Result = Some PlainText|};
+         {|MediaType = "*/*"; Result = Some AnyType|};
+         {|MediaType = "application/vnd.visio"; Result = None|}]
+        |> Seq.iter
+            (fun testCase ->
+                let jsonRequest = fixture.Create<HttpRequestData>()
+                jsonRequest.Headers.Add("Accept", testCase.MediaType)
+                pickReturnMediaType jsonRequest
+                |> should equal testCase.Result)
