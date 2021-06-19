0f0a910a5a4f4e990d13daafd22a82cdeed0d39a
Brad Ackerman <brad@facefault.org>
2021-06-18 19:01:57 -0700

    More ConfigRepository.

--- PoudriereC2/ConfigFiles.fs
+++ PoudriereC2/ConfigFiles.fs
@@ -1,42 +1,28 @@
 namespace Facefault.PoudriereC2
-open Facefault.PoudriereC2.Data
+
 open Facefault.PoudriereC2.Database
 open Facefault.PoudriereC2.Serialization
-open Microsoft.Extensions.Logging
 open Microsoft.Azure.Functions.Worker
 open Microsoft.Azure.Functions.Worker.Http
-open System.Text.Json
-open Npgsql
 open System
 open System.Net
-open System.Linq
 open FSharp.Data.Sql
 
 type ConfigFileApi (db: DB.dataContext, cfg: ConfigRepository) =
 
     [<Function("GetConfigFilesMetadata")>]
-    member this.getConfigFileMetadata
+    member __.getConfigFileMetadata
         ([<HttpTrigger(AuthorizationLevel.Function, "get", Route="configurationfiles/metadata")>]
         req: HttpRequestData) (execContext: FunctionContext) =
             let log = execContext.GetLogger()
             async {
-                let files = Seq.toList <| query {
-                   for file in db.Poudrierec2.Configfiles do
-                   select
-                     { Id = file.Id
-                       Deleted = file.Deleted
-                       Name = file.Name
-                       Portset = file.Portset
-                       Porttree = file.Porttree
-                       Jail = file.Jail
-                       FileType = FromString<ConfigFileType> file.Configtype }
-                }
+                let! files = cfg.getConfigFiles()
                 let response = req.CreateResponse(HttpStatusCode.OK)
                 return response.writeJsonResponse files
             } |> Async.StartAsTask
 
     [<Function("GenerateConfigFile")>]
-    member this.generateConfigFile
+    member __.generateConfigFile
         ([<HttpTrigger(AuthorizationLevel.Function, "get", Route="configurationfiles/{configFile:guid}")>]
          req: HttpRequestData) (execContext: FunctionContext) (configFile: string) =
             async {
--- PoudriereC2/ConfigRepository.fs
+++ PoudriereC2/ConfigRepository.fs
@@ -1,13 +1,14 @@
 namespace Facefault.PoudriereC2
 
 open Facefault.PoudriereC2
+open Facefault.PoudriereC2.Data
 open Facefault.PoudriereC2.Database
 open System
 open FSharp.Data.Sql
 
 type ConfigRepository (db: DB.dataContext) =
 
-    member this.getConfigFileOptions (configFile: string) =
+    member __.getConfigFileOptions (configFile: string) =
         async {
             let! opts =
                 query {
@@ -18,4 +19,21 @@ type ConfigRepository (db: DB.dataContext) =
                              Value = configOption.Value }
                 } |> Seq.executeQueryAsync
             return opts
+        }
+
+    member __.getConfigFiles () =
+        async {
+            let! configFiles =
+                query {
+                    for file in db.Poudrierec2.Configfiles do
+                    select
+                     { Id = file.Id
+                       Deleted = file.Deleted
+                       Name = file.Name
+                       Portset = file.Portset
+                       Porttree = file.Porttree
+                       Jail = file.Jail
+                       FileType = FromString<ConfigFileType> file.Configtype }
+                } |> Seq.executeQueryAsync
+            return configFiles
         }
