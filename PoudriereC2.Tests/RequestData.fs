557035b6785b22405c70cb76076155c509dd39b6
Brad Ackerman <brad@facefault.org>
2021-06-27 13:47:24 -0700

    More progress.

--- PoudriereC2.Tests/RequestData.fs
+++ PoudriereC2.Tests/RequestData.fs
@@ -7,23 +7,11 @@ open System.IO
 
 type ConcreteResponseData(functionContext) =
     inherit HttpResponseData(functionContext)
-    override _.Body
-        with get (): Stream = 
-            failwith "Not Implemented"
-        and set (v: Stream): unit = 
-            failwith "Not Implemented"
+    override val Body = new MemoryStream() :> Stream with get, set
     override _.Cookies: HttpCookies = 
         failwith "Not Implemented"
-    override _.Headers
-        with get (): HttpHeadersCollection = 
-            failwith "Not Implemented"
-        and set (v: HttpHeadersCollection): unit = 
-            failwith "Not Implemented"
-    override this.StatusCode
-        with get (): Net.HttpStatusCode = 
-            failwith "Not Implemented"
-        and set (v: Net.HttpStatusCode): unit = 
-            failwith "Not Implemented"
+    override val StatusCode = Net.HttpStatusCode.OK with get, set
+    override val Headers = HttpHeadersCollection() with get, set
 
 type ConcreteRequestData(functionContext) =
     inherit HttpRequestData(functionContext)
