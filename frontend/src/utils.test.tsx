2047f994a1fd95f983af5f298a3f9c311d25db98
Brad Ackerman <brad@facefault.org>
2021-07-07 22:25:30 -0700

    Less convoluted test.

--- frontend/src/utils.test.tsx
+++ frontend/src/utils.test.tsx
@@ -1,40 +1,31 @@
 import { sortBy } from './utils';
 import * as fc from 'fast-check';
 
-// This test is probably overkill, but Maxim #37 applies.
 it("successfully sorts a list of objects", () => {
-    const objectsToSortArb =
-        // Generate a key to hold the sorting key
-        fc.string({minLength: 1}).chain(sortKey =>
-            // And a random number of objects to sort
-            fc.nat(25).chain((numObjectsToSort) =>
-                // A list of sort key values
-                fc.array(fc.string(), { minLength: numObjectsToSort, maxLength: numObjectsToSort })
-                    .chain(sortKeyValues =>
-                        // Generate the objects to be sorted
-                        fc.array(fc.object({values: [fc.string()]}),
-                            { minLength: numObjectsToSort,
-                                maxLength: numObjectsToSort})
-                            .map((obj) => {
-                                // Put a random string into the sort key of each object
-                                // and return the generated list of objects along with
-                                // the sort key used.
-                                return {
-                                    key: sortKey,
-                                    values: (
-                                        obj.map((rec, i) => {
-                                            rec[sortKey] = sortKeyValues[i];
-                                            return rec;
-                                        }))
-                                }
-                            }))));
+    const arb = fc.string({ maxLength: 1 }).chain(sortKey =>
+        fc.tuple(fc.string(), fc.string()).map(
+            ([a, b]) => {
+                let obja: Record<string, string> = {};
+                let objb: Record<string, string> = {};
+                obja[sortKey] = a;
+                objb[sortKey] = b;
+                return { key: sortKey, values: [obja, objb] };
+            }));
+
     fc.assert(
-        fc.property(objectsToSortArb,
-            (sortees) => {
-                const sortKey = sortees.key;
-                const sorted = sortees.values.sort(sortBy(sortKey));
-                return sorted.every((rec, i) =>
-                    i === (sorted.length - 1) || // skips the last element
-                    (rec[sortKey] as string) <= (sortees.values[i+1][sortKey] as string));
+        fc.property(arb,
+            (testCase) => {
+                const sortA = testCase.values[0];
+                const sortB = testCase.values[1];
+                const sortFunc = sortBy<Record<string, string>, string>(testCase.key);
+                let expected: number;
+                if (sortA[testCase.key] < sortB[testCase.key])
+                    expected = -1;
+                else if (sortA[testCase.key] > sortB[testCase.key])
+                    expected = 1;
+                else
+                    expected = 0;
+                const actual = sortFunc(sortA, sortB);
+                return actual === expected;
             }));
 })
