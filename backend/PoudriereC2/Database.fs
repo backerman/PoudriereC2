03ee1daafdc20537ea5096a90a82d252fbdd0a23
Brad Ackerman <brad@facefault.org>
2021-08-02 22:15:01 -0700

    Factor out DatabaseError logging/sanitizing

--- backend/PoudriereC2/Database.fs
+++ backend/PoudriereC2/Database.fs
@@ -4,6 +4,8 @@ open FSharp.Data.LiteralProviders
 open FSharp.Data.Sql
 open Npgsql
 open System
+open Microsoft.Extensions.Logging
+open System.Net
 
 [<Literal>]
 let ConnectionString =
@@ -42,3 +44,31 @@ type DatabaseError =
                         | _ -> Unknown e
                     | _ -> Unknown e
         }
+
+    /// Handle a DatabaseError, returning a FunctionResult.
+    member dbError.Handle
+        (log: ILogger<obj>,
+         failureMessage: string,
+         [<ParamArray>]failureMessageArgs: obj[])
+            : {| httpCode: HttpStatusCode; result: FunctionResult |} =
+        match dbError with
+        | NoError -> {| httpCode = HttpStatusCode.OK; result = OK |}
+        | ForeignKeyViolation exn ->
+            let userError =
+                match exn.ColumnName with
+                | null -> "A referenced row does not exist."
+                | x -> $"The value of ${x} refers to a nonexistent row."
+            log.LogError(exn, $"${failureMessage}", failureMessageArgs)
+            {| httpCode = HttpStatusCode.BadRequest; result = Error userError |}
+        | UniqueViolation exn ->
+            let userError =
+                match exn.ColumnName with
+                | null -> "This row is a duplicate."
+                | x -> $"There is already a row for this ${x}."
+            log.LogError(exn, failureMessage, failureMessageArgs)
+            {| httpCode = HttpStatusCode.BadRequest; result = Error userError |}
+        | Unknown exn ->
+            // Assume this error contains sensitive data.
+            log.LogError(exn, failureMessage, failureMessageArgs)
+            {| httpCode = HttpStatusCode.InternalServerError
+               result = Error "An internal error occurred." |}
