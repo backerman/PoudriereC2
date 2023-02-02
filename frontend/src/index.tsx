6242d058242f3c81100e621f8ff685fa67f9ca7a
Brad Ackerman <brad@facefault.org>
2023-02-02 13:55:17 -0500

    Switch to createRoot API.

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { App } from './App';
 import { mergeStyles } from '@fluentui/react';
-import reportWebVitals from './reportWebVitals';
 import { initializeIcons } from '@fluentui/react/lib/Icons';
+import { createRoot } from 'react-dom/client';
+import reportWebVitals from './reportWebVitals';
+
+import { App } from './App';
 
 initializeIcons();
 
@@ -16,7 +16,9 @@ mergeStyles({
   },
 });
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const container = document.getElementById('root');
+const root = createRoot(container!);
+root.render(<App />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
