165c2d2ac15c9a8a30bc1f0a47b39379b4540f91
Brad Ackerman <brad@facefault.org>
2023-02-03 16:26:46 -0500

    Remove unused code.

--- frontend/tests/App.spec.ts
+++ frontend/tests/App.spec.ts
@@ -1,11 +1,5 @@
 import { test, expect } from '@playwright/test';
 
-import React from 'react';
-import App from 'src/pages/_app';
-import { initializeIcons } from '@fluentui/react';
-
-initializeIcons();
-
 test('renders nav bar', async ({page}) => {
   await page.goto('http://localhost:3003');
   const linkElement = page.getByText(/Configuration/i);
