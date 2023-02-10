737ec5e999ed96a8410d9c104b333d01a285ecf2
Brad Ackerman <brad@facefault.org>
2023-02-09 21:57:16 -0800

    IOC of header

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -1,4 +1,4 @@
-import { IStackStyles, IStackTokens, Stack } from "@fluentui/react";
+import { getTheme, IStackItemStyles, IStackStyles, IStackTokens, ITextStyles, Stack, Text } from "@fluentui/react";
 import { ReactElement, ReactNode } from "react"
 import { NavMenu } from "./NavMenu"
 
@@ -23,26 +23,46 @@ const contentStyles: Partial<IStackStyles> = {
 }
 
 interface IHasChildren {
-    children?: ReactNode
+  children?: ReactNode
 }
 
-export default function Layout({ children }: IHasChildren) : ReactElement {
-    return (
-      <>
-      <Stack
-        horizontal
-        horizontalAlign="start"
-        tokens={stackTokens}
-        styles={stackStyles}
-        verticalFill={true}>
-        <Stack.Item>
-          <NavMenu />
-        </Stack.Item>
-        <Stack.Item grow styles={contentStyles}>
-          {children}
-        </Stack.Item>
-      </Stack>
-      </>
-    )
+const theme = getTheme();
+const topbarStyles: IStackItemStyles = {
+  root: {
+    background: theme.palette.themePrimary,
+    padding: '0.5rem'
   }
-  
+}
+
+const topbarTextStyles: ITextStyles = {
+  root: {
+    color: theme.palette.white
+  }
+}
+
+export default function Layout({ children }: IHasChildren): ReactElement {
+  return (
+    <>
+      <Stack>
+        <Stack.Item styles={topbarStyles}>
+          <Text variant='large' styles={topbarTextStyles}>
+            Poudriere configuration
+          </Text>
+        </Stack.Item>
+        <Stack
+          horizontal
+          horizontalAlign="start"
+          tokens={stackTokens}
+          styles={stackStyles}
+          verticalFill={true}>
+          <Stack.Item>
+            <NavMenu />
+          </Stack.Item>
+          <Stack.Item grow styles={contentStyles}>
+            {children}
+          </Stack.Item>
+        </Stack>
+      </Stack>
+    </>
+  )
+}
