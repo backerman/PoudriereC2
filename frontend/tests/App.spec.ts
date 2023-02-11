8577c17bbc8e13cde0a7e43fe598e5c30850b120
Brad Ackerman <brad@facefault.org>
2023-02-10 21:14:56 -0800

    Correct test.

--- frontend/tests/App.spec.ts
+++ frontend/tests/App.spec.ts
@@ -2,6 +2,6 @@ import { test, expect } from '@playwright/test';
 
 test('renders nav bar', async ({page}) => {
   await page.goto('http://localhost:3003');
-  const linkElement = page.getByText(/Configuration/i);
+  const linkElement = page.getByRole('link', { name: 'Configuration', exact: true });
   await expect(linkElement).toBeVisible();
 });
