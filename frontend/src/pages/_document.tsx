39a0fd655e07b7a31d4ba0763e0ac55965c6fe89
Brad Ackerman <brad@facefault.org>
2023-02-17 20:23:35 -0800

    Explicitly initialize stylesheets.
    This step is required for production but not development. I have no idea
    why.
    
    Source: https://github.com/microsoft/fluentui/wiki/Server-side-rendering-and-browserless-testing

--- frontend/src/pages/_document.tsx
+++ frontend/src/pages/_document.tsx
@@ -1,15 +1,48 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { InjectionMode, Stylesheet, resetIds } from '@fluentui/react';
+import { NextPage } from 'next';
+import Document, { Html, Head, Main, NextScript, DocumentContext, DocumentInitialProps } from 'next/document'
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-        <NextScript />
-      </body>
-    </Html>
-  )
+const stylesheet = Stylesheet.getInstance();
+
+type MyDocInitialProps = DocumentInitialProps & {
+  serializedStylesheet: string;
+  styleTags: string;
 }
+
+export class MyDocument extends Document<MyDocInitialProps> {
+  static async getInitialProps({ renderPage }: DocumentContext)
+    : Promise<MyDocInitialProps> {
+    resetIds();
+
+    // eslint-disable-next-line react/display-name
+    const page = await renderPage(App => props => {
+      return <App {...props} />;
+    });
+
+    return { ...page,
+      styleTags: stylesheet.getRules(true),
+      serializedStylesheet: stylesheet.serialize() };
+  }
+
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <style type="text/css" dangerouslySetInnerHTML={{ __html: this.props.styleTags }} />
+          <script type="text/javascript" dangerouslySetInnerHTML={{ __html: `
+            window.FabricConfig = window.FabricConfig || {};
+            window.FabricConfig.serializedStylesheet = ${this.props.serializedStylesheet};
+          ` }} />
+        </Head>
+        <body>
+          <Main />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <link rel="icon" href="/favicon.ico" />
+          <NextScript />
+        </body>
+      </Html>
+    )  
+  }
+}
+
+export default MyDocument;
